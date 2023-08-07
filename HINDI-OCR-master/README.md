167b2476d5ca29b5254996e1bcb22cbb1ede17e0
VIKRAM SINGH <75414572+vikrampan@users.noreply.github.com>
2023-08-07 10:31:22 +0530

    Add files via upload

---
+++ HINDI-OCR-master/README.md
@@ -0,0 +1,36 @@
+# Hindi-Character-Recognition
+
+### The app is live on huggingface spaces! Try it -> [Gradio-HCR](https://huggingface.co/spaces/abhiswain/Gradio-Hindi-Character-Recognition)
+
+## Running the app locally:
+
+You can run the streamlit and gradio app locally. 
+
+1. Install the requirements: `pip install -r requirements.txt`
+
+2. Now, just do `streamlit run app.py` or `gradio gradio_app.py`
+
+### Streamlit demo
+
+![App](https://user-images.githubusercontent.com/54038552/210263132-2e95ad65-5049-4a8c-b453-2616cbc4df20.gif)
+
+### Gradio demo
+
+![GradioApp](https://user-images.githubusercontent.com/54038552/220750032-5f7ba8dd-2f70-418c-9752-53bf25aaf8d1.gif)
+
+## Training the model (Optional)
+
+1. Install the requirements: `pip install -r requirements.txt`
+
+2. Hindi Character Recognition
+
+    Getting the data:
+    - Download the data from [here](https://www.kaggle.com/datasets/suvooo/hindi-character-recognition)
+    - Unzip it. You need to split the data into 4 different directories, since we are training for Hindi digits & letters separately.
+    ![image](https://user-images.githubusercontent.com/54038552/209815855-cd629bdd-5a9a-474e-8ad6-1d4df1954fdc.png)
+    
+    How to run ?
+    - You can create your custom model in the `model.py` file or can go with the `HNet` already present. For custom models created, you need to import them to `train.py`, for them to to use. Remember we are training different models for Hindi Digit & Characters.
+    - Now to train the model with default params do, `python train.py`. You can also specify epochs and lr. Most important, is the `model_type`
+    - To train do, `python train.py --epochs <num-epochs> --lr <learning-rate> --model_type <type-of-model>`
+    
---
+++ HINDI-OCR-master/app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import torch
+import src.config as CFG
+from PIL import Image
+import numpy as np
+import json
+from src.model import HNet
+
+
+def classify(model, file, mapping):
+    image = Image.open(file).convert("RGB")
+    image = np.array(image)
+    image = torch.from_numpy(image).float()
+    image = image.permute(2, 0, 1).unsqueeze(0)
+
+    outputs = model(image)
+
+    _, preds = torch.max(outputs, 1)
+
+    st.info(
+        f"The predicted character is: {mapping[str(preds[0].item())]}",
+    )
+
+
+st.markdown(
+    "<h1><i><center>Hindi Character Recognition</center></i><h1>",
+    unsafe_allow_html=True,
+)
+
+option = st.sidebar.radio(
+    label="Classify Hindi Digit or Vyanjan ?", options=["Digit", "Vyanjan"], index=0
+)
+
+
+def upload_and_classify(model, mapping):
+    file = st.file_uploader("Upload image!")
+
+    if file is not None:
+
+        st.image(file, use_column_width=True)
+        button = st.button("Predict")
+
+        if button:
+            with st.spinner("Classifying....."):
+                classify(model=model, file=file, mapping=mapping)
+
+    else:
+
+        digit_url = "https://sample-characters.s3.us-east-2.amazonaws.com/1026.png"
+        vynajan_url = "https://sample-characters.s3.us-east-2.amazonaws.com/5414.png"
+
+        st.info(
+            f"""
+                    -> Download a test [VYANJAN/DIGIT] image from here: [vyanjan.png]({digit_url}) / [digit.png]({vynajan_url}). \n 
+                    -> Select the respective option on the left.
+                    """
+        )
+
+
+mapping, model = None, None
+
+if option == "Digit":
+    if CFG.BEST_MODEL_DIGIT.exists():
+        model = HNet(num_classes=10)
+        model.load_state_dict(torch.load(CFG.BEST_MODEL_DIGIT, map_location=CFG.DEVICE))
+        with open(CFG.INDEX_DIGIT, "r") as f:
+            mapping = json.load(f)
+        upload_and_classify(model, mapping)
+    else:
+        st.error("No model exists! First Train model!")
+else:
+    if CFG.BEST_MODEL_VYANJAN.exists():
+        model = HNet(num_classes=36)
+        model.load_state_dict(
+            torch.load(CFG.BEST_MODEL_VYANJAN, map_location=CFG.DEVICE)
+        )
+        with open(CFG.INDEX_VYNAJAN, "r") as f:
+            mapping = json.load(f)
+        upload_and_classify(model, mapping)
+    else:
+        st.error("No model exists! First train model!")
